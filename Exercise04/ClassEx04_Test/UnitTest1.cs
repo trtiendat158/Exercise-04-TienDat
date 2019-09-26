8d66c010a200dfb1a2fe86d3c3bb7b0f4ad7c266
TranTienDatPM06566 <tiendat.tr158@gmail.com>
2019-09-26 15:16:56 +0700

    abc

---
+++ Exercise04/ClassEx04_Test/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassExercise4a;
+
+namespace ClassEx04_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIsOdd()
+        {
+            bool result3 = Program.IsOdd(3);
+            Assert.AreEqual(true, result3);
+
+            bool result4 = Program.IsOdd(4);
+            Assert.AreEqual(false, result4);
+
+        }
+        [TestMethod]
+        public void TestEven()
+        {
+            bool result2 = Program.IsEven(2);
+            Assert.AreEqual(true, result2);
+
+            bool result5 = Program.IsEven(5);
+            Assert.AreEqual(false, result5);
+        }
+            
+        [TestMethod]
+        public void TestPrime()
+        {
+            bool result3 = Program.IsPrime(13);
+            Assert.AreEqual(true, result3);
+            bool result4 = Program.IsPrime(4);
+            Assert.AreEqual(false, result4);
+        }
+
+        [TestMethod]
+        public void Square()
+        {
+            int result2 = Program.Square(5);
+            Assert.AreEqual(25, result2);
+
+            int result8 = Program.Square(8);
+            Assert.AreEqual(64, result8);
+        }
+
+        [TestMethod]
+        public void Cube()
+        {
+            int result2 = Program.Cube(2);
+            Assert.AreEqual(8, result2);
+
+            int result8 = Program.Cube(3);
+            Assert.AreEqual(27, result8);
+        }
+    }
+}
--- Exercise04/ClassExercise4a/Program.cs
+++ Exercise04/ClassExercise4a/Program.cs
@@ -6,13 +6,47 @@ using System.Threading.Tasks;
 
 namespace ClassExercise4a
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Tien Dat Tr"); 
         }
 
-        
+        public static bool IsOdd(int n)
+        {
+            if (n % 2 != 0)
+                return true;
+            else return false;
+        }
+
+       public static bool IsEven(int n)
+        {
+            if (n % 2 == 0)
+                return true;
+            else return false;
+        }
+
+       public static bool IsPrime(int n)
+       {
+           if (n == 1) return true;
+           for(int i=2; i<n; i++)
+           {
+               if(n % i ==0)
+               return false;
+           }
+           return true;
+       }
+
+       public static int Square(int n)
+       {
+
+           return n * n;
+       }
+
+       public static int Cube(int n)
+       {
+           return n * n * n;
+       }
     }
 }
