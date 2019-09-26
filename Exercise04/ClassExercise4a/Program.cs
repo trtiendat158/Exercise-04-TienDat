2fb1034d29389cbd5e5141c3b8b800b27c6b7419
TranTienDatPM06566 <tiendat.tr158@gmail.com>
2019-09-26 14:26:49 +0700

    abc

---
+++ Exercise04/ClassExercise4a/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercise4a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tien Dat Tr"); 
+        }
+
+        
+    }
+}
